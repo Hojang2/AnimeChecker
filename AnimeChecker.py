c9bb5f6884d42b15ce555875c999c6e92607c637
Pavel Balusek <mr.p.balusek@gmail.com>
2018-02-22 09:55:38 +0100

    First working basic database

--- AnimeChecker.py
+++ AnimeChecker.py
@@ -1,40 +1,65 @@
 #!/usr/bin/env python3
 import tkinter
-import DatabaseHandler
+from DatabaseHandler import DatabaseHandler
 from tkinter.font import Font
 from tkinter import ttk
+
 
 class Checker(tkinter.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.parent.title("Pro moje anime")
+        self.parent.title("Moje anime")
         self.parent.minsize(900, 600)
         self.parent.resizable(True, True)
+        self.dHandler = DatabaseHandler()
+        self.data = self.dHandler.return_database()
         self.create_widgets()
 
+    def add_new_anime(self):
+        if self.new_anime.get() != "":
+            self.dHandler.add_new_anime_to_db(self.new_anime.get())
+            self.data = self.dHandler.return_database()
+            self.show_anime.config(values=self.data)
+            self.new_anime.delete(0, 'end')
+
+    def delete_anime(self):
+        self.dHandler.delete_datebase()
+        self.data = self.dHandler.return_database()
+        self.show_anime.config(values=self.data)
+
     def create_widgets(self):
-        self.label = tkinter.Label(text="Nejoblíbenější Anime", font=Font(size=50))
-        self.label.grid(row=0, column=0, columnspan=4, rowspan=1)
+        label = tkinter.Label(text="Nejoblíbenější Anime", font=Font(size=50))
+        label.grid(row=0, column=0, columnspan=5, rowspan=1)
 
-        self.label = tkinter.Label(text="Zadej nové anime které chceš sledovat")
-        self.label.grid(row=2, column=0)
+        label = tkinter.Label(text="Přidat nové Anime", font=Font(size=40))
+        label.grid(row=1, column=0, columnspan=5, rowspan=1)
 
-        self.label = tkinter.Entry()
-        self.label.grid(row=2, column=1)
+        label = tkinter.Label(text="Zadej název nového anime které chceš sledovat")
+        label.grid(row=2, column=0)
 
-        self.label = tkinter.Label(text="Anime které sleduješ:")
-        self.label.grid(row=2, column=2)
+        self.new_anime = tkinter.Entry(self.parent)
+        self.new_anime.grid(row=2, column=1)
+        confirm_newA_button = tkinter.Button(self.parent, text="Přidat anime",
+                                             command=lambda: self.add_new_anime())
+        confirm_newA_button.grid(row=2, column=2)
 
-        self.combobox = ttk.Combobox(self.parent)
-        self.combobox.grid(row=2,column=3)
+        label = tkinter.Label(text="Nejnovější díly", font=Font(size=40))
+        label.grid(row=4, column=0, columnspan=5, rowspan=1)
 
-        self.label = tkinter.Label(text="Nejnovější díly", font=Font(size=40))
-        self.label.grid(row=3, column=0, columnspan=4, rowspan=1)
+        label = tkinter.Label(text="Anime které sleduješ:")
+        label.grid(row=5, column=0)
 
-        self.label = tkinter.Label(text="Vybrané anime", font=Font(size=40))
-        self.label.grid(row=4, column=0, columnspan=4, rowspan=1)
+        print(self.data)
+        self.show_anime = ttk.Combobox(self.parent, values=self.data)
+        self.show_anime.grid(row=5, column=1)
 
+        delete_anime_button = tkinter.Button(self.parent, text="Smazat všechny anime",
+                                             command=lambda: self.delete_anime(), anchor="center")
+        delete_anime_button.grid(row=6, column=0, columnspan=5)
+
+        label = tkinter.Label(text="Vybrané anime", font=Font(size=40))
+        label.grid(row=7, column=0, columnspan=5, rowspan=1)
 root = tkinter.Tk()
 app = Checker(root)
 app.mainloop()
--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -1,3 +1,41 @@
-class DatabaseHandler():
+import io
+class DatabaseHandler:
+
     def __init__(self):
-        pass
+        name_database = open("AnimeDatabase/MainAnime.txt", mode="r")
+        self.refractore_names = name_database.read().splitlines()
+        name_database.close()
+
+    def add_new_anime_to_db(self, anime=""):
+        name_database = open("AnimeDatabase/MainAnime.txt", mode="w")
+        if(self.check_database(self.refractore_names, anime) != True):
+            self.refractore_names.append(anime)
+            anime_database = open("AnimeDatabase/" + anime + ".txt", mode="w")
+            anime_database.close()
+        for str in self.refractore_names:
+            name_database.write(str+"\n")
+        name_database.close()
+
+    def delete_datebase(self):
+        name_database = open("AnimeDatabase/MainAnime.txt", mode="w")
+        for i in range(len(self.refractore_names)):
+            name_database.seek(i)
+            name_database.truncate()
+        self.refractore_names = []
+        name_database.close()
+
+    def return_database(self):
+        return self.refractore_names
+
+    def check_database(self, database, name=""):
+        for anime in database:
+            if anime == name:
+                return True
+        return False
+
+    def check_symbol(self, string="", symbol=""):
+        for a in string:
+            if a == symbol:
+                    return True
+        return False
+
