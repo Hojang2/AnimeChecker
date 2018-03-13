42dfb777e75d286273b72692672b8fbe5b3cb2c2
Pavel Balusek <mr.p.balusek@gmail.com>
2018-03-13 07:31:20 +0100

    Opraveno pro spustitelnost v exe

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -4,7 +4,7 @@ class DatabaseManager:
         self.info_list = ["Žánr knihy:", "Specifický literární žánr:", "Členění textu:", "Forma vypravěče:",
                           "Typ vypravěče:", "Typ řečí hlavních postav:", "Autor:", "Doba vzniku díla:", "Literární směr:",
                           "Další představitelé:", "Další díla:", "Podobné dílo:", "Čas v knize:", "Zápletka díla:", "Začátek:", "Konec:",
-                           "Místo příběhu:", "Hlavní postavy:",]
+                           "Místo příběhu:", "Hlavní postavy:"]
 
     def add_new_anime_to_db(self, book="", createFile=True):
         refractore_names = self.return_name_database()
@@ -26,7 +26,7 @@ class DatabaseManager:
         name_database.close()
 
     def remove_file(self, name=""):
-        os.remove("DiaryDatabase/" + name + ".txt")
+        os.remove(name + ".txt")
 
     def delete_name(self, name=""):
         refractore_names2 = []
@@ -43,31 +43,32 @@ class DatabaseManager:
 
 
     def write_to_file(self, name="", data=[]):
-        anime_data = open("DiaryDatabase/" + name + ".txt", mode="w")
+        anime_data = open(name + ".txt", mode="w")
         for i, data in enumerate(data):
             anime_data.write(self.info_list[i]+" "+str(data)+"\n")
         anime_data.close()
 
     def read_from_file(self, file_name):
-        anime_data=open("DiaryDatabase/" + file_name + ".txt", mode="r")
+        anime_data=open(file_name + ".txt", mode="r")
         return anime_data.read().splitlines()
 
     def get_info(self):
         return self.info_list
 
     def return_name_database(self):
+        self.is_file_there()
         name_database = open("MainDiaryDetails.txt", mode="r")
-        if name_database != None:
-            refractore_names = name_database.read().splitlines()
-            print(refractore_names)
+        refractore_names = name_database.read().splitlines()
+        name_database.close()
+        return refractore_names
+
+    def is_file_there(self):
+        try:
+            name_database = open("MainDiaryDetails.txt", mode="r")
             name_database.close()
-            return refractore_names
-        else:
-            name_database.close()
+        except:
             name_database2 = open("MainDiaryDetails.txt", mode="w")
             name_database2.close()
-            return []
-
 
     def check_database(self, database, name=""):
         for anime in database:
@@ -83,7 +84,7 @@ class DatabaseManager:
 
     def load_book(self, name=""):
         try:
-            name_database = open("DiaryDatabase/" + name + ".txt", mode="r")
+            name_database = open(name + ".txt", mode="r")
             self.add_new_anime_to_db(name, createFile=False)
         except:
             print ("Soubor neexistuje")
